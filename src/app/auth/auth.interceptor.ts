47d5d32f971ea10542aff2346010258b6da3efba
Magzhan Turdygazin <turdygazin.magzhan@alser.kz>
2025-02-12 14:30:50 +0500

    добавил rxjs, router

---
+++ src/app/auth/auth.interceptor.ts
@@ -0,0 +1,15 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { AuthService } from './auth.service';
+import { inject } from '@angular/core';
+
+export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = inject(AuthService).token;
+
+  if (!token) {
+    return next(req);
+  }
+
+  req.clone({
+    setHeaders
+  })
+};
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../auth/auth.service';
+import { delay, from, map, pipe, skip, take, tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrl: './login-page.component.scss',
+  standalone: true,
 })
 export class LoginPageComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
 
+  form = new FormGroup({
+    username: new FormControl<string>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    password: new FormControl<string>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+  });
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.authService.login(this.form.getRawValue()).subscribe((res) => {
+        this.router.navigate(['']);
+      });
+    }
+  }
 }
